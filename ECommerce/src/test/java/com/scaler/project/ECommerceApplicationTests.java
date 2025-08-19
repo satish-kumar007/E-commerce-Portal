e79243b249ab473c7166e9da70494b65b7decef9
Satish Kumar <javadev.satish@gmail.com>
2025-08-19 11:45:06 +0530

    Ecommerce project created.

---
+++ ECommerce/src/test/java/com/scaler/project/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
